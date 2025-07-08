7f3f30d82d9141701aaddede22dd02f58b9b8706
igorkll <frerther@gmail.com>
2025-07-08 16:36:13 +0300

    try use default lua library (ffi broken)

--- extensionSources/BetterRender/BetterRender/extension.lua
+++ extensionSources/BetterRender/BetterRender/extension.lua
@@ -1,38 +1,3 @@
 local better, directory = ...
 
-local ffi = require("ffi")
-local BetterRender = ffi.load(directory .. "BetterRender.dll")
-
-ffi.cdef[[
-typedef struct BetterRender BetterRender;
-
-BetterRender* create(int width, int height);
-void destroy(BetterRender* betterRender);
-
-void begin_draw(BetterRender* betterRender);
-void draw_clear(BetterRender* betterRender, uint32_t color);
-void draw_pixel(BetterRender* betterRender, int posX, int posY, uint32_t color);
-void end_draw(BetterRender* betterRender);
-
-void begin_read(BetterRender* betterRender);
-uint32_t read_pixel(BetterRender* betterRender, int width, int height);
-void end_read(BetterRender* betterRender);
-
-int test(int test);
-]]
-
-better.render = {
-    create = BetterRender.create,
-    destroy = BetterRender.destroy,
-
-    begin_draw = BetterRender.begin_draw,
-    draw_clear = BetterRender.draw_clear,
-    draw_pixel = BetterRender.draw_pixel,
-    end_draw = BetterRender.end_draw,
-
-    begin_read = BetterRender.begin_read,
-    read_pixel = BetterRender.read_pixel,
-    end_read = BetterRender.end_read,
-
-    test = BetterRender.test
-}
+better.render = require("BetterRender")
--- extensionSources/BetterRender/Source/main.c
+++ extensionSources/BetterRender/Source/main.c
@@ -14,6 +14,9 @@
 #include <math.h>
 #include <sys/stat.h>
 #include <rlights.h>
+#include <luajit/lua.h>
+#include <luajit/lauxlib.h>
+#include <luajit/lualib.h>
 
 #define PACKED __attribute__((packed))
 #define EXPORT __declspec(dllexport)
@@ -49,53 +52,102 @@ static Color unpackColor(uint32_t color) {
 
 // -----------------------------------
 
-EXPORT BetterRender* create(int width, int height) {
+static int create(lua_State* L) {
     init();
+
+    int width = luaL_checkinteger(L, 1);
+    int height = luaL_checkinteger(L, 2);
 
     BetterRender* betterRender = malloc(sizeof(BetterRender));
     betterRender->renderTarget = LoadRenderTexture(width, height);
 
-    return betterRender;
+    lua_pushlightuserdata(L, (void*)betterRender);
+    return 1;
 }
 
-EXPORT void destroy(BetterRender* betterRender) {
+static int destroy(lua_State* L) {
+    BetterRender* betterRender = (BetterRender*)lua_touserdata(L, 1);
+
     UnloadRenderTexture(betterRender->renderTarget);
     free(betterRender);
+
+    return 0;
 }
 
 // -----------------------------------
 
-EXPORT void begin_draw(BetterRender* betterRender) {
+static int begin_draw(lua_State* L) {
+    BetterRender* betterRender = (BetterRender*)lua_touserdata(L, 1);
     BeginTextureMode(betterRender->renderTarget);
+    return 0;
 }
 
-EXPORT void draw_clear(BetterRender* betterRender, uint32_t color) {
+static int draw_clear(lua_State* L) {
+    BetterRender* betterRender = (BetterRender*)lua_touserdata(L, 1);
+    int64_t color = luaL_checkinteger(L, 2);
+
     ClearBackground(unpackColor(color));
+    return 0;
 }
 
-EXPORT void draw_pixel(BetterRender* betterRender, int x, int y, uint32_t color) {
+static int draw_pixel(lua_State* L) {
+    BetterRender* betterRender = (BetterRender*)lua_touserdata(L, 1);
+    int x = luaL_checkinteger(L, 2);
+    int y = luaL_checkinteger(L, 3);
+    int64_t color = luaL_checkinteger(L, 4);
+
     DrawPixel(x, y, unpackColor(color));
+    return 0;
 }
 
-EXPORT void end_draw(BetterRender* betterRender) {
+static int end_draw(lua_State* L) {
     EndTextureMode();
+    return 0;
 }
 
 // -----------------------------------
 
-EXPORT void begin_read(BetterRender* betterRender) {
+static int begin_read(lua_State* L) {
+    BetterRender* betterRender = (BetterRender*)lua_touserdata(L, 1);
     betterRender->image = LoadImageFromTexture(betterRender->renderTarget.texture);
+    return 0;
 }
 
-EXPORT uint32_t read_pixel(BetterRender* betterRender, int x, int y) {
+static int read_pixel(lua_State* L) {
+    BetterRender* betterRender = (BetterRender*)lua_touserdata(L, 1);
+    int x = luaL_checkinteger(L, 2);
+    int y = luaL_checkinteger(L, 3);
     Color color = GetImageColor(betterRender->image, x, y);
-    return packColor(color);
+    lua_pushinteger(L, packColor(color));
+    return 1;
 }
 
-EXPORT void end_read(BetterRender* betterRender) {
+static int end_read(lua_State* L) {
+    BetterRender* betterRender = (BetterRender*)lua_touserdata(L, 1);
     UnloadImage(betterRender->image);
+    return 0;
 }
 
-EXPORT int test(int test) {
-    return test * 2;
+// -----------------------------------
+
+static const struct luaL_Reg g_functions[] = {
+    {"create", create},
+    {"destroy", destroy},
+
+    {"begin_draw", begin_draw},
+    {"draw_clear", draw_clear},
+    {"draw_pixel", draw_pixel},
+    {"end_draw", end_draw},
+
+    {"begin_read", begin_read},
+    {"read_pixel", read_pixel},
+    {"end_read", end_read},
+
+    {NULL, NULL}
+};
+
+EXPORT int luaopen_BetterRender(lua_State * L) {
+    luaL_register(L, "BetterRender", g_functions);
+
+    return 1;
 }
