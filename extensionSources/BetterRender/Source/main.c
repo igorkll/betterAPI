949844dd2c18d5725c0401880625d2e6c299704e
igorkll <frerther@gmail.com>
2025-07-25 08:32:41 +0300

    Update main.c

--- extensionSources/BetterRender/Source/main.c
+++ extensionSources/BetterRender/Source/main.c
@@ -20,12 +20,52 @@
 
 // -----------------------------------
 
+static IDXGISwapChain* gameSwapChain = NULL;
 
+static void _init() {
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+
+    const char CLASS_NAME[] = "DummyWindowClass";
+    WNDCLASS wc = {0};
+    wc.lpfnWndProc = NULL;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0, CLASS_NAME, "Dummy Window", WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 100, 100,
+        NULL, NULL, hInstance, NULL
+    );
+
+    if (hwnd == NULL) {
+        return;
+    }
+
+    DXGI_SWAP_CHAIN_DESC swapChainDesc = {0};
+    swapChainDesc.BufferCount = 1;
+    swapChainDesc.BufferDesc.Width = 100;
+    swapChainDesc.BufferDesc.Height = 100;
+    swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.OutputWindow = hwnd;
+    swapChainDesc.SampleDesc.Count = 1;
+    swapChainDesc.Windowed = TRUE;
+
+    IDXGISwapChain* swapChain = NULL;
+
+    HRESULT hr = CreateDXGIFactory(&IID_IDXGIFactory, (void**)&swapChain);
+    if (SUCCEEDED(hr)) {
+        gameSwapChain = swapChain - 1;
+        if (swapChain) swapChain->Release();
+    }
+}
 
 // -----------------------------------
 
 static const struct luaL_Reg g_functions[] = {
-
+    {"_init", _init},
+    
     {NULL, NULL}
 };
 
